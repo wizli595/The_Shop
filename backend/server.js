91e067821c61013397b27b3cfed8307dcdf9d923
wizli595 <abdoowizli@gmail.com>
2023-11-22 11:45:42 +0100

    server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("hhhhhhhhh");
+import express from "express";
+import products from "./data/products.js";
+const app = express();
+const PORT = 5000;
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+app.get("/api/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.find((e) => e._id === id);
+  res.json(product);
+});
+app.listen(PORT, () => {
+  console.log("App running on port" + PORT);
+});
