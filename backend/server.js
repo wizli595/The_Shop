497824f8919906d03a5b56d46de7f61deaadf7c1
wizli595 <abdoowizli@gmail.com>
2023-12-06 12:55:45 +0100

    all basic done

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,8 @@ app.get("/api/config/paypal", (req, res) => {
   });
 });
 
-const __dirname = path.resolve(); // Set __dirname to current directory
+// Set __dirname to current directory
+const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // If no route work or the above throw an error
