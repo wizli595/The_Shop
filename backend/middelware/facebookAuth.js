f7b26814667f2a52da3cc795328772a1fe3d7d4a
wizli595 <abdoowizli@gmail.com>
2024-01-22 17:39:18 +0100

    package fix

--- backend/middelware/facebookAuth.js
+++ backend/middelware/facebookAuth.js
@@ -11,7 +11,7 @@ export const configureFacebookAuth = () => {
       {
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
-        callbackURL: "http://localhost:5000/auth/facebook/callback",
+        callbackURL: "https://jawhara.onrender.com/auth/facebook/callback",
         profileFields: ["id", "displayName", "photos", "email"],
       },
       asyncHandler(async (accessToken, refreshToken, profile, done) => {
